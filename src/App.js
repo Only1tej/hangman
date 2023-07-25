54e3556853363ee6bf81c33936f556cbe3bb66af
only1tej <tijanienny3@gmail.com>
2023-07-24 22:31:02 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+    </>
+  );
+}
+
+export default App;
