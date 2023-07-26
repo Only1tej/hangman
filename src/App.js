e42d17983aaa6619f93789e6b298c17aef25050d
only1tej <tijanienny3@gmail.com>
2023-07-25 20:12:55 -0700

    Final commit

--- src/App.js
+++ src/App.js
@@ -42,6 +42,16 @@ function App() {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [correctLetters, wrongLetters, playable]);
 
+  function playAgain() {
+    setPlayable(true);
+    //Empty arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
   return (
     <>
       <Header />
@@ -55,6 +65,7 @@ function App() {
         wrongLetters={wrongLetters}
         selectedWord={selectedWord}
         setPlayable={setPlayable}
+        playAgain={playAgain}
       />
       <Notification showNotification={showNotification} />
     </>
